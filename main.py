7fb8a53786a9794217da7f46fcf5307e76cc0c40
Jayesh <jayesh@example.com>
2026-04-01 17:41:57 +0530

    Day 2

--- main.py
+++ main.py
@@ -2,7 +2,30 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "First Request"}
+todos = []
 
+@app.get("/todos") # get all todo lists
+def get_todo():
+    return todos
+
+@app.get("/todos/{todo_id}") # get todo, id vice
+def dfs(todo_id:int):
+    for to in todos:
+        if to["id"] == todo_id:
+            return to
+
+@app.post("/todos") # make a todo
+def sub_todo(item: str):
+    todo = {
+        "id" : len(todos) + 1,
+        "task" : item
+    }
+    todos.append(todo)
+    return todo
+
+@app.delete("/todos/{todo_id}") # delete a todo
+def dlt_todo(todo_id: int):
+    for to in todos:
+        if to["id"] == todo_id:
+            todos.remove(to)
+            return {"Message" : "Removed"}
